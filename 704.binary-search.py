b662256030d41271d93faed596bca059a72a0293
John Song <jstarts86@gmail.com>
2025-05-04 22:05:40 +0900

    binary search done

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -1,23 +1,38 @@
 # @leet start
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        # mid 
-        mid_index = len(nums)/2
-    
+        def search_recursive(low,high):
+            # mid 
+            mid_index =  (high + low) // 2
 
-        # left half
-    
+            value = nums[mid_index]
+
+            if low > high:
+                return -1
+
+            if value == target:
+                return mid_index
+
+            elif value > target:
+                return search_recursive(low,mid_index)
+            else:
+                return search_recursive(mid_index,high)
+        return search_recursive(0, len(nums)-1)
 
 
         # Right half
-        return nums
+        # return nums
         
 # @leet end
 #
-list1 = [-1,0,3,5,9,12]
+list1 = [-1,0,1,3,5,9,12,]
 
 length = len(list1)
 
-middle_index = length/2
-print(middle_index)
+solution = Solution()
+
+print("Solution is ",solution.search(list1, 9))
+
+# middle_index = length//2
+# print(middle_index)
 
